0ca9641198325126742241a11e33c9379c9d3655
taro-git <t.taro30954694@gmail.com>
2026-02-23 23:06:47 +0900

    feat: add teams services and repository in batch server

--- server/src/common/types.py
+++ server/src/common/types.py
@@ -13,6 +13,15 @@ class Conference(Enum):
     west = "West"
     east = "East"
 
+    @classmethod
+    def from_str(cls, conference_str: str) -> "Conference":
+        """
+        カンファレンスを示す文字列から Conference Enum を返します.
+        """
+        if conference_str.lower().startswith("e"):
+            return cls.east
+        return cls.west
+
 
 class Division(Enum):
     """
@@ -25,3 +34,20 @@ class Division(Enum):
     northwest = "NorthWest"
     pacific = "Pacific"
     southwest = "SouthWest"
+
+    @classmethod
+    def from_str(cls, division_str: str) -> "Division":
+        """
+        ディビジョンを示す文字列から Division Enum を返します.
+        """
+        if division_str.lower().startswith("a"):
+            return cls.atlantic
+        if division_str.lower().startswith("c"):
+            return cls.central
+        if division_str.lower().startswith("n"):
+            return cls.northwest
+        if division_str.lower().startswith("p"):
+            return cls.pacific
+        if "east" in division_str.lower() or "se" in division_str.lower():
+            return cls.southeast
+        return cls.southwest
