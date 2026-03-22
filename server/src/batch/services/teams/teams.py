361d9a4190e63b1e0d1ab001b204b8ea2818129a
taro-git <t.taro30954694@gmail.com>
2026-03-22 15:44:30 +0900

    chore: change data source for team property

--- server/src/batch/services/teams/teams.py
+++ server/src/batch/services/teams/teams.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from typing import TypedDict
 
 from nba_api.stats.endpoints.leaguestandingsv3 import LeagueStandingsV3
 from nba_api.stats.endpoints.scheduleleaguev2 import ScheduleLeagueV2
@@ -13,57 +14,93 @@ from common.types import Conference, Division
 logger = logging.getLogger(__name__)
 
 
-def _create_team_tricode(season: Season) -> dict[int, str]:
+class LeagueScheduleV2Response(TypedDict):
+    teamName: str | None
+    teamTricode: str | None
+    teamCity: str | None
+
+
+class LeagueStandingsV3Response(TypedDict):
+    Conference: str
+    Division: str
+
+
+def _get_team_props_from_league_schedule(season: Season) -> dict[int, LeagueScheduleV2Response]:
     """
-    指定したシーズンに試合が予定されているチームのチームIDと略称の一覧を作成します.
+    試合日程を基に、指定したシーズンに試合が予定されているチームのチームIDとチーム情報の一覧を作成します.
     """
     league_schedule = NbaApiGateway().fetch(ScheduleLeagueV2, season=season.season_str).get("leagueSchedule", {})
     season_games = league_schedule.get("gameDates", [])
 
-    team_dict: dict[int, str] = {}
+    props: dict[int, LeagueScheduleV2Response] = {}
 
     for day_data in season_games:
         for game in day_data.get("games", []):
             for side in ("awayTeam", "homeTeam"):
                 team = game.get(side, {})
-                team_id = team.get("teamId")
-                tricode = team.get("teamTricode")
+                team_id = team["teamId"]
+                props[team_id] = {
+                    "teamName": team.get("teamName", None),
+                    "teamTricode": team.get("teamTricode", None),
+                    "teamCity": team.get("teamCity", None),
+                }
+    return props
 
-                if team_id and tricode:
-                    team_dict[team_id] = tricode
-    return team_dict
+
+def _get_team_props_from_league_standings(season: Season) -> dict[int, LeagueStandingsV3Response]:
+    """
+    リーグ順位を基に、指定したシーズンに試合が予定されているチームのチームIDとチーム情報の一覧を作成します.
+    """
+    league_standings = NbaApiGateway().fetch(LeagueStandingsV3, season=season.season_str).get("resultSets", [])[0]
+    standings = league_standings.get("rowSet", [])
+    headers = league_standings.get("headers", [])
+    props: dict[int, LeagueStandingsV3Response] = {}
+    for s in standings:
+        team_id = s[headers.index("TeamID")]
+        props[team_id] = {
+            "Conference": s[headers.index("Conference")],
+            "Division": s[headers.index("Division")],
+        }
+    return props
 
 
 def _create_teams_by_season(season: Season) -> list[Team]:
     """
     シーズンを指定して、チーム一覧を作成します.
     """
-    result_set = NbaApiGateway().fetch(LeagueStandingsV3, season=season.season_str).get("resultSets", [])[0]
-    standings = result_set.get("rowSet", [])
-    headers = result_set.get("headers", [])
-    return [Team(id=s[headers.index("TeamID")]) for s in standings]
+    return [Team(id=id) for id in _get_team_props_from_league_schedule(season).keys() if id != 0]
 
 
 def _create_team_properties_by_season(season: Season) -> list[TeamProperty]:
     """
     シーズンを指定して、チーム情報一覧を作成します.
     """
-    result_set = NbaApiGateway().fetch(LeagueStandingsV3, season=season.season_str).get("resultSets", [])[0]
-    standings = result_set.get("rowSet", [])
-    headers = result_set.get("headers", [])
-    team_tricode = _create_team_tricode(season)
-    return [
-        TeamProperty(
-            team_id=s[headers.index("TeamID")],
-            season=season.start_year,
-            team_name=s[headers.index("TeamName")],
-            team_tricode=team_tricode[s[headers.index("TeamID")]],
-            conference=Conference.from_str(s[headers.index("Conference")]),
-            division=Division.from_str(s[headers.index("Division")]),
-            team_city=s[headers.index("TeamCity")],
-        )
-        for s in standings
-    ]
+    team_props_from_league_standings = _get_team_props_from_league_standings(season)
+    team_props_from_league_schedule = _get_team_props_from_league_schedule(season)
+    team_properties: list[TeamProperty] = []
+    for team_id, team_props in team_props_from_league_schedule.items():
+        if team_id in team_props_from_league_standings:
+            conference = Conference.from_str(team_props_from_league_standings[team_id]["Conference"])
+            division = Division.from_str(team_props_from_league_standings[team_id]["Division"])
+        else:
+            conference = None
+            division = None
+        name = team_props["teamName"]
+        tricode = team_props["teamTricode"]
+        city = team_props["teamCity"]
+        if team_id != 0 and name is not None and tricode is not None and city is not None:
+            team_properties.append(
+                TeamProperty(
+                    team_id=team_id,
+                    season=season.start_year,
+                    team_name=name,
+                    team_tricode=tricode,
+                    conference=conference,
+                    division=division,
+                    team_city=city,
+                )
+            )
+    return team_properties
 
 
 def sync_teams_by_season(season: Season | None = None) -> None:
